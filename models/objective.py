2e5011c092be375ad0c689f52250a5f9f01d1e23
virajjadeja <75031921+virajjadeja@users.noreply.github.com>
2021-07-06 17:38:50 +0530

    Update objective.py

--- models/objective.py
+++ models/objective.py
@@ -2,6 +2,7 @@ from odoo import api, fields, models,_
 from odoo.exceptions import ValidationError,RedirectWarning,UserError
 import re
 from datetime import datetime, timedelta
+from datetime import datetime, timedelta
 
 class Objective(models.Model):
     _name= "objective.objective"
@@ -10,11 +11,10 @@ class Objective(models.Model):
     
  
     objective_title_id = fields.Char(string='Objective Title')
-    start_date = fields.Date(string="Starting Date")
+    start_date = fields.Date(string="Starting Date",default=datetime.today())
     end_date = fields.Date(string="Ending Date")
     total_hour = fields.Float(string='Total Hours')
     progress = fields.Float(string='Progress %')
-    company = fields.Char(string='Company')
     purpose = fields.Char(string='Purpose')
     remark = fields.Text(string='Remarks')
     objective_ids = fields.One2many('objective.line' ,'objective_id')
@@ -26,10 +26,11 @@ class ObjectiveLine(models.Model):
     _description='ObjectiveLine'
 
     objective_id = fields.Many2one('objective.objective',string="Objective")
-    key_result = fields.Char("Key Result")
-    employee = fields.Float("Employee")
+    key_result = fields.Many2one('key.result',"Key Result")
+    employee = fields.Many2one('res.partner',"Employee")
     progress = fields.Float(string='Progress %')
     total_hour = fields.Float(string='Total Hours')
+    
     
     
 
