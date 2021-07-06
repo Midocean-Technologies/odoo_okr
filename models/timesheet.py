45b9d51a8e3b556defe680a7f2c1ef7ae1563071
virajjadeja <75031921+virajjadeja@users.noreply.github.com>
2021-07-06 17:39:57 +0530

    Update timesheet.py

--- models/timesheet.py
+++ models/timesheet.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models,_
 from odoo.exceptions import ValidationError,RedirectWarning,UserError
 import re
+from datetime import datetime, timedelta
 
 class Timesheet(models.Model):
     _name= "timesheet.timesheet"
@@ -8,10 +9,9 @@ class Timesheet(models.Model):
     _rec_name = 'employee_id'
 
 
-    company = fields.Char(string='Company')
-    employee = fields.Char(string='Employee')
+    employee = fields.Many2one('res.partner',string='USER')
     employee_id = fields.Many2one('res.partner',string='Employee Name')
-    date = fields.Date(string=" Date")
+    date = fields.Date(string=" Date",default=datetime.today())
     percent = fields.Float(string='DSP Percent')
     timesheet_ids = fields.One2many('timesheet.line' ,'timesheet_id')
    
@@ -22,10 +22,11 @@ class TimesheetLine(models.Model):
     _description='Timesheet Line'
 
     timesheet_id = fields.Many2one('timesheet.timesheet',string="Timesheet")
-    start_time = fields.Float("Start Time")
-    end_time = fields.Float("End Time")
+    start_time = fields.Date("Start Time" ,default=datetime.today())
+    end_time = fields.Date("End Time")
     status = fields.Many2one('okr.status',"Status")
-    key_result = fields.Char('Key Result')
+    key_result = fields.Many2one('key.result','Key Result')
+
 
    
 
