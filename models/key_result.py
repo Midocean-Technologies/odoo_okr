da259b8d39ee34593cd1cd521faef156cc46c3b8
Sagar <bhogayata71@gmail.com>
2021-07-06 18:14:04 +0000

    New Changes

--- models/key_result.py
+++ models/key_result.py
@@ -5,8 +5,9 @@ import re
 class Key(models.Model):
     _name= "key.result"
     _description='Key Result'
-    _rec_name = 'employee_id'
- 
+    _rec_name = 'key_result_title'
+    
+    key_result_title = fields.Char(string='Key Result Title')
     key_result_progress = fields.Float(string='Key Result Progress ')
     employee = fields.Many2one('res.partner',string='Employee')
     employee_id = fields.Many2one('res.partner',string='Employee Name')
--- models/timesheet.py
+++ models/timesheet.py
@@ -9,7 +9,7 @@ class Timesheet(models.Model):
     _rec_name = 'employee_id'
 
 
-    employee = fields.Many2one('res.partner',string='USER')
+    employee = fields.Many2one('res.users',string='User', default=lambda self: self.env.user)
     employee_id = fields.Many2one('res.partner',string='Employee Name')
     date = fields.Date(string=" Date",default=datetime.today())
     percent = fields.Float(string='DSP Percent')
@@ -22,8 +22,8 @@ class TimesheetLine(models.Model):
     _description='Timesheet Line'
 
     timesheet_id = fields.Many2one('timesheet.timesheet',string="Timesheet")
-    start_time = fields.Date("Start Time" ,default=datetime.today())
-    end_time = fields.Date("End Time")
+    start_time = fields.Datetime("Start Time")
+    end_time = fields.Datetime("End Time")
     status = fields.Many2one('okr.status',"Status")
     key_result = fields.Many2one('key.result','Key Result')
 
